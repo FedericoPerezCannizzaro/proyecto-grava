6998b5e8180c31836e49bce538270d6b72ef50ba
Federico Perez Cannizzaro <47121530+FedericoPerezCannizzaro@users.noreply.github.com>
2019-05-06 12:39:58 -0300

    Add files via upload

---
+++ proyecto/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('URL', 'http://localhost/proyecto/');
+
+define('HOST', 'localhost');
+define('DB', 'mvc');
+define('USER', 'root');
+define('PASSWORD', "123123");
+define('CHARSET', 'utf8mb4');
+
+?>
